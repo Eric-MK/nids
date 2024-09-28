db80b405e226ad57582a0109db24839ba8977bb4
unknown <mutungaeric839@gmail.com>
2024-09-28 13:42:09 +0300

    cnn input

--- cnn_lstm_model.py
+++ cnn_lstm_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import h5py
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, LSTM, Dense, concatenate, Flatten
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, LSTM, Dense, concatenate, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.metrics import classification_report, confusion_matrix
@@ -18,22 +18,25 @@ def load_data(file_path):
     return X_cnn, X_lstm, y
 
 # Load the datasets
-train_file = './output_cnn_lstm/10t-10n-DOS2019-dataset-train-cnn-lstm.hdf5'
-val_file = './output_cnn_lstm/10t-10n-DOS2019-dataset-val-cnn-lstm.hdf5'
-test_file = './output_cnn_lstm/10t-10n-DOS2019-dataset-test-cnn-lstm.hdf5'
+train_file = './output_cnn_lstm/10t-100n-DOS2019-dataset-train-cnn-lstm.hdf5'
+val_file = './output_cnn_lstm/10t-100n-DOS2019-dataset-val-cnn-lstm.hdf5'
+test_file = './output_cnn_lstm/10t-100n-DOS2019-dataset-test-cnn-lstm.hdf5'
 
 X_train_cnn, X_train_lstm, y_train = load_data(train_file)
 X_val_cnn, X_val_lstm, y_val = load_data(val_file)
 X_test_cnn, X_test_lstm, y_test = load_data(test_file)
 
+print("CNN input shape:", X_train_cnn.shape)
+print("LSTM input shape:", X_train_lstm.shape)
+
 # Define the CNN+LSTM hybrid model
 def create_cnn_lstm_model(input_shape_cnn, input_shape_lstm):
     # CNN part
     cnn_input = Input(shape=input_shape_cnn, name='cnn_input')
-    x = Conv1D(64, 3, activation='relu')(cnn_input)
-    x = MaxPooling1D(2)(x)
-    x = Conv1D(128, 3, activation='relu')(x)
-    x = MaxPooling1D(2)(x)
+    x = Conv2D(64, (3, 3), activation='relu')(cnn_input)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(128, (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
     x = Flatten()(x)
 
     # LSTM part
@@ -57,7 +60,7 @@ model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', m
 
 # Define callbacks
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-model_checkpoint = ModelCheckpoint('best_model.h5', save_best_only=True)
+model_checkpoint = ModelCheckpoint('best_model.keras', save_best_only=True)
 
 # Train the model
 history = model.fit(
